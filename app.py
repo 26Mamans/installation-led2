d1d51ed642867718c3bb8d04067b04d71f70e419
Ruben <rubaise770@gmail.com>
2025-11-19 20:16:25 +0100

    Mise à jour app.py pour gérer le formulaire POST et Excel

--- app.py
+++ app.py
@@ -6,12 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('formulaire.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    # Ici on ne fait rien, juste rediriger vers merci
-    return "<h1>Merci ! Votre formulaire a été soumis ✅</h1>"
-
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
